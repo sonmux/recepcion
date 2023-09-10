0da0f82b6e1de6996e275bb69feee54d5b5b72a7
Anggelo <son.anggelo@gmail.com>
2023-09-10 17:24:05 -0600

    pdf firma

--- Aplicación/docs front/src/AcuerdoForm/CreateAcuerdo.js
+++ Aplicación/docs front/src/AcuerdoForm/CreateAcuerdo.js
@@ -1,57 +1,118 @@
-import React, { useState } from 'react';
+//* importar axios
+import axios from "axios";
+//* importar react-router-dom
+import {useNavigate} from 'react-router-dom'
+import React, { useState, useRef } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
-import DraggableSignature from 'react-digital-signature';
+import SignatureCanvas from 'react-signature-canvas';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import './styles.css';
+import { PDFDocument } from 'pdf-lib';
 
+// Configura la ubicación del worker de pdf.js
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+const URI = 'http://localhost:8000/acuerdo'
 
-
-function App() {
+const App = () => {
   const [pdfFile, setPdfFile] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
-  const [signature, setSignature] = useState(null);
-
-  const onDocumentLoadSuccess = ({ numPages }) => {
-    setPageNumber(1);
-  };
+  const signatureRef = useRef();
+  const [numPages, setNumPages] = useState(null);
+  const navigate = useNavigate()
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
-    setPdfFile(file);
+    setPdfFile(URL.createObjectURL(file));
   };
 
-  const handleSignature = (signatureDataUrl) => {
-    setSignature(signatureDataUrl);
+  const handleClearSignature = () => {
+    signatureRef.current.clear();
   };
 
-  const downloadSignedPDF = () => {
-    // Aquí debes implementar la lógica para descargar el PDF firmado
-    // utilizando pdf-lib y ofrecerlo como una descarga para el usuario.
-  };
+  const handleSaveSignature = async () => {
+    const signatureImage = signatureRef.current.toDataURL();
+  
+    const originalPdfBytes = await fetch(pdfFile).then((res) => res.arrayBuffer());
+    const pdfDoc = await PDFDocument.load(new Uint8Array(originalPdfBytes));
+    const [page] = pdfDoc.getPages();
+  
+    const image = await pdfDoc.embedPng(signatureImage);
+    const { width, height } = image.size();
+    const x = 100; // Posición X de la firma
+    const y = 100; // Posición Y de la firma
+  
+    page.drawImage(image, {
+      x,
+      y,
+      width: width * 0.5,
+      height: height * 0.5,
+    });
+  
+    const modifiedPdfBytes = await pdfDoc.save();
+    const modifiedPdfBlob = new Blob([modifiedPdfBytes], { type: 'application/pdf' });
+    setPdfFile(URL.createObjectURL(modifiedPdfBlob));
+    let binaryString = '';
+        modifiedPdfBytes.forEach((byte) => {
+        binaryString += String.fromCharCode(byte);
+    });
 
-  const savePDFToMySQL = () => {
-    // Aquí debes implementar la lógica para convertir el PDF con firma a Base64
-    // y enviarlo a tu servidor Express con Axios para guardarlo en MySQL.
+    // Convierte la cadena a Base64
+    const base64modifiedPdf = btoa(binaryString);
+
+    console.log(base64modifiedPdf); // Muestra la cadena Base64 en la consola
+  
+    await axios.post(URI,{
+      acuerdo:base64modifiedPdf,
+      idCliente:1
+    })
+  };
+  
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+    setPageNumber(1);
   };
 
   return (
     <div>
-      <h1>Firma Digital en React</h1>
+      <h1>Aplicación de Firma Electrónica</h1>
       <input type="file" accept=".pdf" onChange={handleFileChange} />
       {pdfFile && (
         <div>
           <Document file={pdfFile} onLoadSuccess={onDocumentLoadSuccess}>
             <Page pageNumber={pageNumber} />
           </Document>
-          <DraggableSignature onEnd={handleSignature} 
-            style={{display:"inline-block", 
-            border:"1px "}}
-          />
-          <button onClick={downloadSignedPDF}>Descargar PDF Firmado</button>
-          <button onClick={savePDFToMySQL}>Guardar en MySQL</button>
+          <div>
+            <p>
+              Página {pageNumber} de {numPages}
+            </p>
+            <button
+              onClick={() => setPageNumber(pageNumber - 1)}
+              disabled={pageNumber <= 1}
+            >
+              Anterior
+            </button>
+            <button
+              onClick={() => setPageNumber(pageNumber + 1)}
+              disabled={pageNumber >= numPages}
+            >
+              Siguiente
+            </button>
+          </div>
+          <div>
+            <SignatureCanvas
+              ref={signatureRef}
+              canvasProps={{ width: 400, height: 200 }}
+            />
+          </div>
+          <div>
+            <button onClick={handleClearSignature}>Borrar Firma</button>
+            <button onClick={handleSaveSignature}>Guardar Firma como Base64</button>
+          </div>
         </div>
       )}
     </div>
   );
-}
+};
 
 export default App;
--- Aplicación/node/database/db.js
+++ Aplicación/node/database/db.js
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
-const db = new Sequelize('recepcion', 'root', '',{
+const db = new Sequelize('recepcion', 'root', '1234',{
     host:'localhost',
     dialect:'mysql'
 });
